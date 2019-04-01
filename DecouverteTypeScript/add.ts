ba2d71fcb3de725ca6fa4b36b9df6df65480cf55
quentin <q.thibaut.lazaro96@gmail.com>
2019-04-01 10:26:17 +0200

    Decouverte_TypeScript

---
+++ DecouverteTypeScript/add.ts
@@ -0,0 +1,15 @@
+function hello(name: string) {
+     console.log("Hello " + name);
+ }
+
+ let firstName: string = "bob";
+
+ hello(firstName);
+ hello(firstName + " marley");
+
+ function concat(a: string, b: string): string  {
+     return a + b;
+ }
+
+ let wcs: string = concat("Wild", concat("Code", "School"));
+ console.log(wcs); 
